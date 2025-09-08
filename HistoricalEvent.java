505a96a1963c6ecd8c0b043824c4c9786b9c686a
Robert Sistek <155775913+CashlessElf@users.noreply.github.com>
2025-09-08 21:44:27 +0000

    Completed Getters and Setters for both new created classes.

--- HistoricalEvent.java
+++ HistoricalEvent.java
@@ -1,16 +1,33 @@
 public class HistoricalEvent {
     private String description;
     private Date eventDay;
-    
 
+    //Setters
+    public void setDescription(String description) {
+        this.description = description;
+    }
 
+    public void setEventDay(Date eventDay) {
+        this.eventDay = eventDay;
+    }
+
+    //Getters
+    public String getDescription() {
+        return description;
+    }
+
+    public String getEventDay() {
+        return toString(eventDay);
+    }
+
+    //Other Methods
     @Override
 	public String toString()
     {
         return this.description + " " + this.eventDay + " ";
 
     }
-    
+    //Equals
     @Override
 	public boolean equals(Object other)
 	{
--- RevisedHistoricalEvent.java
+++ RevisedHistoricalEvent.java
@@ -6,13 +6,28 @@ public class RevisedHistoricalEvent extends HistoricalEvent {
         System.out.print("The following historical event was originally described as such: \n" + "\n"+ HistoricalEvent.description);
         
         System.out.print("\n Now in the modern era there has been an attempt to rewrite history as it had been reported on initaly to push a more modern narrative. Here is this revisonist history: \n" + "\n" + revisedDescription);
-
-
     }
-    
+    //Setters
+    public void setRevisedDescription(String revisedDescription) {
+        this.revisedDescription = revisedDescription;
+    }
+
+    public void setCitation(String citation) {
+        this.citation = citation;
+    }
+
+    //Getters
+    public String getRevisedDescription() {
+        return revisedDescription;
+    }
+
+    public String getCitation() {
+        return citation;
+    }
 
 
 
+    //Other Methods
     @Override
 	public String toString()
     {
