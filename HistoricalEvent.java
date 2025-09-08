fd6699f47439cb07ba97a0f08bff4acba41ac3a8
Robert Sistek <155775913+CashlessElf@users.noreply.github.com>
2025-09-08 21:09:30 +0000

    Have started setting up Equals and Two String methods for the two created classes.

--- HistoricalEvent.java
+++ HistoricalEvent.java
@@ -1,4 +1,34 @@
 public class HistoricalEvent {
     private String description;
-    private Date eventDay; 
+    private Date eventDay;
+    
+
+
+    @Override
+	public String toString()
+    {
+        return this.description + " " + this.eventDay + " ";
+
+    }
+    
+    @Override
+	public boolean equals(Object other)
+	{
+		Date eventDay;
+		
+		if(other == null)
+		{
+			return false;
+		}
+		else if(! (other instanceof Date)) 
+		{
+			return false;
+		}
+		else
+		{
+			eventDay = (Date) other;
+			return this.eventDay.equals(otherDate.eventDay);
+		}
+	}
+
 }
--- RevisedHistoricalEvent.java
+++ RevisedHistoricalEvent.java
@@ -3,7 +3,38 @@ public class RevisedHistoricalEvent extends HistoricalEvent {
     private String citation;
 
     public void teach() {
-        
+
     }
+    
+
+
+
+    @Override
+	public String toString()
+    {
+        return this.revisedDescription + " " + this.citation + " ";
+
+    }
+    
+    @Override
+	public boolean equals(Object other)
+	{
+		Date eventDay;
+		
+		if(other == null)
+		{
+			return false;
+		}
+		else if(! (other instanceof Date)) 
+		{
+			return false;
+		}
+		else
+		{
+			eventDay = (Date) other;
+			return this.eventDay.equals(otherDate.eventDay);
+		}
+	}
+
     
 }
